950c5c7d54ad86dcdb0d5385cfa76a46dd4f04d3
Khushi Bhadauria <khushibhadauria189@gmail.com>
2025-06-30 13:05:50 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-length = int(input("Enter desired password length: "))
-password = ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
-print("Generated Password:", password)
